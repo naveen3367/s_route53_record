87805f6de09b0998665dc4af7489371e88c3020f
Nav <naveen3367@gmail.com>
2023-09-21 08:11:56 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "record_name" {
+  description = "The DNS record name."
+}
+
+variable "record_value" {
+  description = "The DNS record value (e.g., IP address)."
+}
+
+variable "zone_id" {}
+
+variable "record_type" {}
+
+variable "ttl" {}
